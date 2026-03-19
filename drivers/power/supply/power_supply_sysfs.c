799db6e01d108e79286a0e4fdf0e5023f844797d
Rocky7842 <eric.rocky7842@gmail.com>
2026-03-19 21:10:46 +0800

    power: supply: sysfs: Fix power_supply_type
    * Re-import cleaned OSRC G980FXXUFHWAK changes to upstream to solve
      text mismatch.
    
    Change-Id: Ieabd50f085608014eadc3feaee40b4b036eec441

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -43,10 +43,13 @@ static struct device_attribute power_supply_attrs[];
 static const char * const power_supply_type_text[] = {
 	"Unknown", "Battery", "UPS", "Mains", "USB",
 	"USB_DCP", "USB_CDP", "USB_ACA", "USB_C",
-	"USB_PD", "USB_PD_DRP", "BrickID",
-	"USB_HVDCP", "USB_HVDCP_3", "USB_HVDCP_3P5", "Wireless", "USB_FLOAT",
-	"BMS", "Parallel", "Main", "USB_C_UFP", "USB_C_DFP",
-	"Charge_Pump",
+	"USB_PD", "USB_PD_DRP", "BrickID", "BMS", "MISC",
+	"Wireless", "HV_Wireless", "PMA_Wireless", "CARDOCK", "UARTOFF", "OTG", "LAN_HUB",
+	"MHL_500", "MHL_900", "MHL_1500", "MHL_USB",
+	"SMART_OTG", "SMART_NOTG", "POWER_SHARING",
+	"HV_Mains", "HV_Mains_12V", "HV_Prepare_Mains", "HV_ERR", "MHL_USB_100", "MHL_2000",
+	"HV_Unknown", "MDOCK_TA", "HMT_CONNECTED", "HMT_CHARGE", "Wireless_Pack", "Wireless_Pack_TA",
+	"Wireless_Stand", "HV_Wireless_Stand", "PDIC", "HV_Mains_CHG_LIMIT",
 };
 
 static const char * const power_supply_usb_type_text[] = {
